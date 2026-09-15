06ed499cde51c66f39e84ff263aac160603103b9
High Spirit Commercial Ventures Private Limited <admin@hscvpl.com>
2026-09-15 11:19:00 +0530

    fix(junior,nav): update category images for backpacks & combo set, shift brand toggle further left in navbar

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -210,7 +210,7 @@ export const Header = ({ onSearchOpen }: { onSearchOpen: () => void }) => {
 
         {/* Desktop Action Icons & Fixed BrandToggle */}
         <div className="hidden lg:flex flex-1 items-center justify-end font-outfit gap-3 xl:gap-4">
-          <div className="mr-3 xl:mr-5">
+          <div className="mr-8 lg:mr-14 xl:mr-20">
             <BrandToggle />
           </div>
           <div className="h-5 w-px bg-black/15 dark:bg-white/20 mx-1" />
--- frontend/src/pages/JuniorPage.tsx
+++ frontend/src/pages/JuniorPage.tsx
@@ -14,8 +14,8 @@ const AGE_GROUPS = [
 ];
 
 const CATEGORIES = [
-  { label: 'School Backpacks', filter: 'school-backpacks', image: '/junior/Junior 1.jpg' },
-  { label: 'Combo Set', filter: 'combo-set', image: '/junior/Rectangle 28.png' },
+  { label: 'School Backpacks', filter: 'school-backpacks', image: '/junior/backapck.jpg' },
+  { label: 'Combo Set', filter: 'combo-set', image: '/junior/Combo set.jpg' },
   { label: 'Trolley Backpacks', filter: 'trolley-backpacks', image: '/junior/Speedo_ Hero 1.png' },
 ];
 
